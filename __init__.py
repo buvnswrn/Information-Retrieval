ec56257daa42e6f6a7bf3212bb95f9a9d93ad18a
Bhuvaneshwaran <bhuvan.nav@gmail.com>
2020-08-01 18:30:33 +0200

    added python file

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+materials = "materials"
+patterns = materials+"/patterns.txt"
+questions = materials+"/test_questions.txt"
+corpus_xml = materials+"/trec_documents.xml"
+
